431df38780771cbf15858bf593e0bd0f4fe1a5fe
Student User <student@workstation.lab.example.com>
2021-05-09 13:00:08 -0400

    fixed application code

--- movies/src/main/java/com/redhat/movies/MoviesController.java
+++ movies/src/main/java/com/redhat/movies/MoviesController.java
@@ -13,7 +13,6 @@ public class MoviesController {
 
     private List<Movie> movies;
     private String status = "OK";
-    private String flag = "READY";
 
     @GetMapping("/movies")
     public List<Movie> getAllMovies() {
